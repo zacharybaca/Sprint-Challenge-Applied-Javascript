5340c80d00590f5f070f4c60d7f4d4a5162d3730
zacharybaca <baca.zach@icloud.com>
2019-09-06 13:03:57 -0500

    Completed Most of The Sprint

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,65 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+        const articles = response.data.articles;
+        const cards = document.querySelector('.cards-container');
+        //Iterate Through Each Category
+        articles.bootstrap.forEach(card => {
+            cards.appendChild(createCard(card.headLine, card.image, card.author));
+        })
+
+        articles.javascript.forEach(card => {
+            cards.appendChild(createCard(card.headLine, card.image, card.author));
+        })
+
+        articles.technology.forEach(card => {
+            cards.appendChild(createCard(card.headLine, card.image, card.author));
+        })
+
+        articles.jquery.forEach(card => {
+            cards.appendChild(createCard(card.headLine, card.image, card.author));
+        })
+
+        articles.node.forEach(card => {
+            cards.appendChild(createCard(card.headLine, card.image, card.author));
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+function createCard(headLine, image, author) {
+    //Create Elements
+    const card = document.createElement('div');
+     headLine = document.createElement('div');
+     author = document.createElement('div');
+    const imageContainer = document.createElement('div');
+     image = document.createElement('img');
+    const byAuthor = document.createElement('span');
+    
+
+    //Assign Classes
+    card.classList.add('card');
+    headLine.classList.add('.headline');
+    author.classList.add('.author');
+    imageContainer.classList.add('img-container');
+
+    //Add Text Content
+    headLine.textContent = headLine;
+    image.src = image;
+    author.textContent = author;
+
+    //Link Elements
+    card.appendChild(headLine);
+    card.appendChild(author);
+    author.appendChild(imageContainer);
+    author.appendChild(byAuthor);
+    imageContainer.appendChild(image);
+
+
+    return card;
+}
